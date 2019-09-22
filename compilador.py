e3a01b2236e4d15d45ebb9f48fa1e0dee1170d4d
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2019-09-21 21:46:29 -0300

    Add files via upload

--- compilador.py
+++ compilador.py
@@ -4,44 +4,26 @@ from PIL import ImageTk, Image # Icone
 import os
 
 tela = Tk()
-tela.title('Linguagem ec beta 0.1')
-tela.call('wm', 'iconphoto', tela._w, ImageTk.PhotoImage(Image.open('icone.gif'))) # Icone
+tela.title('Linguagem ec beta 0.2')
+tela.call('wm', 'iconphoto', tela._w, ImageTk.PhotoImage(Image.open('icone.gif')))
 tela.configure(bg='#343434')
 
+########################### DEFINIÇÃO DE VARIAVEIS GLOBAIS ########################
+
 global btn_ativado # Botão compilar clicado
+global txTelaLogs  # Text de logs
 global aconteceu   # Caso o print(' para colocar ')
 global filename    # Arquivo
 global programa    # Novo código
 global text        # Texto do arquivo
-global st          # Text orincipal
 global p
 
 aconteceu = [False,'']
 btn_ativado = True
 filename = ''
 programa = ''
-st = ''
+txTelaLogs = ''
 p = 0
-
-def abrir_arquivo(filename):
-    try:
-        a = open(filename,'r',encoding="utf8")
-        s = a.read() + ' '
-        a.close()
-        return s
-    except:
-        a = open(filename,'r')
-        s = a.read() + ' '
-        a.close()
-        return s
-
-def salvarArquivo():
-    global text
-    global filename
-    if filename != () and filename != '':
-        a = open(filename, "w")
-        a.write(pr.get(1.0,END))
-        a.close()
 
 def dialog_salvar_arquivo():
     global filename
@@ -50,11 +32,11 @@ def dialog_salvar_arquivo():
     if f is None:
         return
 
-    text2save = str(pr.get(1.0, END))
+    text2save = str(txTelaProgramacao.get(1.0, END))
     f.write(text2save)
     f.close()
     filename = f.name
-    abrir_arquivo(filename)
+    print(filename)
 
 def dialog_abrir_arquivo():
     global filename
@@ -65,174 +47,44 @@ def dialog_abrir_arquivo():
     filename = dlg.show()
 
     if filename != ():
-        text = abrir_arquivo(filename)
-        pr.delete(1.0, END)
-        pr.insert(END, text)
-        atualizar_sintaxe()
+        print('arquivo escolhido')
 
-def escrever_codigo():
-    f = open('programa.py','w',encoding='utf8')
-    f.write("# encoding: utf-8\nimport time\n"+programa)
-    f.close()
-
-def log(texto):
-    global st
-    st.insert(END,str(texto)+"\n")
-    st.see("end")
-    st.update()
-
-def IniciarCompilador():
-    global s
-    global filename
-    log('abrindo programa em altissimo nível')
-    s = abrir_arquivo(filename)
-    return s
-
-def substitui(valor,por):
-    global aconteceu
-    global p
-    global programa
-    if valor == s[p:p+len(valor)]:
-        programa += por
-        p += len(valor)
-        aconteceu = [True,por]
-
-def troca(p,s):
-    substitui('vale','=')
-    substitui('recebe','=')
-    substitui('é igual a','=')
-
-    substitui('enquanto','while')
-
-    substitui(' e ',' and ')
-    substitui(' ou ',' or ')
-    substitui('for diferente de','!=')
-
-    substitui('for diferente de','!=')
-    substitui('for menor que','<')
-    substitui('for maior que','>')
-    substitui('for igual a','==')
-
-    substitui('exiba nessa linha','print(')
-    substitui('exiba','print(')
-    substitui('mostre nessa linha ','print(')
-    substitui('mostre','print(')
-
-    substitui('senao','else:')
-    substitui('se ','if ')
-    substitui('digitado','input()')
-
-    substitui('espere ','time.sleep(')
-
-def Iniciar():
-    log('Convertendo para linguagem de alto nível')
-    global aconteceu
-    global programa
-    global s
-    global p
-
-    while p < len(s):
-        # troca valores
-        troca(p , len(s))
-
-        # se não aconteceu nada
-        if aconteceu[0] == False:
-            programa += s[p]
-            p += 1
-
-        elif aconteceu[1] == 'time.sleep(':
-           inicio = s[ p : p+s[p:].find('segundos')+1]
-           inicio = inicio.replace('segundos',')\n')
-           s = s[:p] + inicio + s[p+s[p:].find('segundos')+1:]
-
-        elif aconteceu[1] == 'print(':
-           inicio = s[ p : p+s[p:].find('\n')+1]
-           inicio = inicio.replace('\n',')\n')
-           s = s[:p] + inicio + s[p+s[p:].find('\n')+1:]
-
-        elif aconteceu[1] == 'print( ':
-           inicio = s[ p : p+s[p:].find('\n')+1]
-           inicio = inicio.replace('\n',',end=" ")\n')
-           s = s[:p] + inicio + s[p+s[p:].find('\n')+1:]
-
-        elif aconteceu[1] == 'if ' or aconteceu[1] == 'while':
-           inicio = s[ p : p+s[p:].find('\n')+1]
-           inicio = inicio.replace('\n',':\n')
-           s = s[:p] + inicio + s[p+s[p:].find('\n')+1:]
-        aconteceu[0] = False
-
-    log('programa em python concluido')
-    escrever_codigo()
-
-def GerarProgramaExecutavel():
-    import os
-    os.system('pyinstaller --onefile programa.py')
-
-def GerarProgramaLinux():
-    import os
-    os.system('cxfreeze programa.py --target-dir Linux')
-    
-def Compilar():
-    global btn_ativado
-    btn_ativado = False
-    IniciarCompilador()
-    Iniciar()
-    btn_ativado = True
-
-def executar_script():
-    global btn_ativado
-    btn_ativado = False
-    IniciarCompilador()
-    Iniciar()
-    btn_ativado = True
-    os.system('python3.6 programa.py')
-    #os.system('python3 programa.py')
-    
-def executar():
-    IniciarCompilador()
-    global btn_ativado
-    if btn_ativado == True:
-        import threading
-        t = threading.Thread(target=Compilar)
-        t.start()
-
-# COR SINTAXE
 def sintaxe_linha(palavra,cor,frase,linha):
     conteudo = frase
     if palavra in frase:
-        pr.tag_add(palavra, linha+str('.')+str(conteudo.find(palavra)), linha+str('.')+str(conteudo.find(palavra)+len(palavra)))
-        pr.tag_config(palavra, foreground=cor)
+        txTelaProgramacao.tag_add(palavra, linha+str('.')+str(conteudo.find(palavra)), linha+str('.')+str(conteudo.find(palavra)+len(palavra)))
+        txTelaProgramacao.tag_config(palavra, fg=cor)
 
-def sintaxe(palavra,cor,pr):
-    lista = pr.get(1.0,END).split('\n')
+def sintaxe(palavra,cor,txTelaProgramacao):
+    lista = txTelaProgramacao.get(1.0,END).split('\n')
     for linha in range(len(lista)):
         sintaxe_linha(palavra,cor,lista[linha],str(linha+1))
 
 def atualizar_sintaxe():
-    sintaxe('vale','orange',pr)
-    sintaxe('recebe','orange',pr)
-    sintaxe('é igual a','orange',pr)
+    sintaxe('vale','orange',txTelaProgramacao)
+    sintaxe('recebe','orange',txTelaProgramacao)
+    sintaxe('é igual a','orange',txTelaProgramacao)
 
-    sintaxe('enquanto','#fe468a',pr)
-    sintaxe(' e ',     '#f9264a',pr)
-    sintaxe(' ou ',    '#f9264a',pr)
+    sintaxe('enquanto','#fe468a',txTelaProgramacao)
+    sintaxe(' e ',     '#f9264a',txTelaProgramacao)
+    sintaxe(' ou ',    '#f9264a',txTelaProgramacao)
   
-    sintaxe('for diferente de','#66d9ef',pr)
-    sintaxe('for menor que',   '#66d9ef',pr)
-    sintaxe('for maior que',   '#66d9ef',pr)
-    sintaxe('for igual a',     '#66d9ef',pr)
+    sintaxe('for diferente de','#66d9ef',txTelaProgramacao)
+    sintaxe('for menor que',   '#66d9ef',txTelaProgramacao)
+    sintaxe('for maior que',   '#66d9ef',txTelaProgramacao)
+    sintaxe('for igual a',     '#66d9ef',txTelaProgramacao)
 
-    sintaxe('exiba nessa linha',  '#22ee22',pr)
-    sintaxe('exiba',              '#22ee22',pr)
-    sintaxe('mostre nessa linha ','#22ee22',pr)
-    sintaxe('mostre',             '#22ee22',pr)
+    sintaxe('exiba nessa linha',  '#22ee22',txTelaProgramacao)
+    sintaxe('exiba',              '#22ee22',txTelaProgramacao)
+    sintaxe('mostre nessa linha ','#22ee22',txTelaProgramacao)
+    sintaxe('mostre',             '#22ee22',txTelaProgramacao)
 
-    sintaxe('senao','#e2d872',pr)
-    sintaxe('se','#e2d872',pr)
-    sintaxe('digitado','#e2d872',pr)
+    sintaxe('senao','#e2d872',txTelaProgramacao)
+    sintaxe('se','#e2d872',txTelaProgramacao)
+    sintaxe('digitado','#e2d872',txTelaProgramacao)
 
-    sintaxe('espere','#fc00ff',pr)
-    sintaxe('segundos','#fc00ff',pr)
+    sintaxe('espere','#fc00ff',txTelaProgramacao)
+    sintaxe('segundos','#fc00ff',txTelaProgramacao)
 
 from design import design
 
@@ -242,63 +94,108 @@ tela.rowconfigure(1,weight=1)
 menubar = Menu(tela,design.cor_menu())
 tela.config(menu=menubar)
 
-menu_arquivo  = Menu(menubar,design.cor_menu())
-menu_compilar = Menu(menubar,design.cor_menu())
-menu_executar = Menu(menubar,design.cor_menu())
-menu_paleta   = Menu(menubar,design.cor_menu())
-menu_ajuda    = Menu(menubar,design.cor_menu())
-menu_sobre    = Menu(menubar,design.cor_menu())
+menu_arquivo     = Menu(menubar,design.cor_menu())
+menu_executar    = Menu(menubar,design.cor_menu())
+menu_localizar   = Menu(menubar,design.cor_menu())
+menu_editar      = Menu(menubar,design.cor_menu())
+menu_ferramentas = Menu(menubar,design.cor_menu())
+menu_interface   = Menu(menubar,design.cor_menu())
+menu_ajuda       = Menu(menubar,design.cor_menu())
+menu_sobre       = Menu(menubar,design.cor_menu())
 
-menubar.add_cascade(label='Arquivo' , menu=menu_arquivo)
-menubar.add_cascade(label='Compilar', menu=menu_compilar)
-menubar.add_cascade(label='Executar', menu=menu_executar)
-menubar.add_cascade(label='Cores'   , menu=menu_paleta)
-menubar.add_cascade(label='Ajuda'   , menu=menu_ajuda)
-menubar.add_cascade(label='sobre'   , menu=menu_sobre)
+menubar.add_cascade(label='Arquivo'   , menu=menu_arquivo)
+menubar.add_cascade(label='Executar'  , menu=menu_executar)
+menubar.add_cascade(label='Localizar' , menu=menu_localizar)
+menubar.add_cascade(label='Editar'    , menu=menu_editar)
+menubar.add_cascade(label='Interface' , menu=menu_interface)
+menubar.add_cascade(label='Ajuda'     , menu=menu_ajuda)
+menubar.add_cascade(label='sobre'     , menu=menu_sobre)
 
-menu_arquivo.add_command(label='Abrir...',command=dialog_abrir_arquivo)
-menu_arquivo.add_command(label='Salvar',command=salvarArquivo)
+############################### ARQUIVO #######################################
+menu_arquivo.add_command(label='Abrir arquivo (Ctrl+O)',command=dialog_abrir_arquivo)
+menu_arquivo.add_command(label='Nova Guia (Ctrl-N)')
+menu_arquivo.add_command(label='Abrir pasta')
+menu_arquivo.add_command(label='Recentes')
 menu_arquivo.add_separator()
-menu_arquivo.add_command(label='Salvar Como',command=dialog_salvar_arquivo)
-menu_arquivo.add_command(label='Sair')
-menu_compilar.add_command(label='Compilar',command=executar)
-menu_executar.add_command(label='Executar',command=executar_script)
-menu_paleta.add_command(label='Azul')
-menu_paleta.add_command(label='Vermelho')
-menu_paleta.add_command(label='Preto')
-menu_ajuda.add_command(label='Ajuda')
+menu_arquivo.add_command(label='Salvar (Ctrl-S)',)
+menu_arquivo.add_command(label='Salvar Como (Ctrl-Shift-S)',command=dialog_salvar_arquivo)
+menu_arquivo.add_separator()
+menu_arquivo.add_command(label='imprimir (Ctrl-P)')
+menu_arquivo.add_command(label='Exportar (Ctrl-E)')
+menu_arquivo.add_command(label='Enviar por e-mail ')
+menu_arquivo.add_separator()
+menu_arquivo.add_command(label='Sair (Alt-F4)')
+
+############################### EXECUTAR #######################################
+menu_executar.add_command(label='Executar Tudo (F5)')
+menu_executar.add_command(label='Executar linha (F6)')
+menu_executar.add_command(label='Executar até breakpoint (F7)')
+menu_executar.add_command(label='Executar com delay (F8)')
+menu_executar.add_command(label='Parar execução (F9)')
+menu_executar.add_command(label='Inserir breakpoint (F10)')
+
+############################### LOCALIZAR #######################################
+menu_localizar.add_command(label='Localizar (CTRL + F)')
+menu_localizar.add_command(label='Substituir (CTRL + R)')
+
+############################### EDITAR #######################################
+menu_editar.add_command(label='copiar (CTRL + C)')
+menu_editar.add_command(label='cortar (CTRL + X)')
+menu_editar.add_command(label='colar (CTRL + V)')
+menu_editar.add_command(label='desfazer (CTRL + Z)')
+menu_editar.add_command(label='refazer (CTRL + Y)')
+menu_editar.add_command(label='selecionar tudo (CTRL + A)')
+    
+############################### FERRAMENTAS #######################################
+menu_ferramentas.add_command(label='corrigir identação')
+menu_ferramentas.add_command(label='Numero de espaços para o tab')
+
+############################### INTERFACE #######################################
+menu_interface.add_command(label='temas')
+menu_interface.add_command(label='fonte')
+
+############################### Ajuda #######################################
+menu_ajuda.add_command(label='Ajuda (F1)')
+menu_ajuda.add_command(label='Comandos Disponíveis')
+menu_ajuda.add_command(label='Comunidade')
+
+############################### Sobre #######################################
 menu_sobre.add_command(label='Projeto')
-menu_sobre.add_command(label='Desenvolvedores',command=lambda:trocar_de_tela(fr,frConfig))
+menu_sobre.add_command(label='Desenvolvedores',command=lambda:trocar_de_tela(frTelaPrincipal,frConfig))
 
 def trocar_de_tela(fechar,carregar):
     fechar.grid_forget()
     carregar.grid(row=1,column=1,sticky=NSEW)
 
-#|--------
-fr = Frame(tela)
-fr.grid_columnconfigure((0,1),weight=2)
-fr.rowconfigure(1,weight=1)
-st = Text(fr,width=40,bg='#343434',fg='#ffffff',font=('',15))
-st.grid(row=1,column=0,sticky=NSEW)
+# INTERFACE GERAL
+frTelaPrincipal = Frame(tela)
+frTelaPrincipal.grid_columnconfigure((0,1),weight=2)
+frTelaPrincipal.rowconfigure(1,weight=1)
 
-pr = Text(fr,bg='#343434',fg='#ffffff',font=('',15))
-pr.grid(row=1,column=1,sticky=NSEW)
-pr.bind("<Key>",lambda pr:atualizar_sintaxe())
-pr.delete(1.0, END)
-fr.grid(row=1,column=1,sticky=NSEW)
+# TELA DE LOGS
+txTelaLogs = Text(frTelaPrincipal,width=40,bg='#343434',fg='#ffffff',font=('',15))
 
-#|--------
+# TELA DE CODIFICAÇÃO
+txTelaProgramacao = Text(frTelaPrincipal,bg='#343434',fg='#ffffff',font=('',15))
+txTelaProgramacao.bind("<Key>",lambda txTelaProgramacao:atualizar_sintaxe())
+txTelaProgramacao.delete(1.0, END)
+
+txTelaLogs.grid(row=1,column=0,sticky=NSEW)
+txTelaProgramacao.grid(row=1,column=1,sticky=NSEW)
+frTelaPrincipal.grid(row=1,column=1,sticky=NSEW)
+
+## ************************************** ##
 frConfig = Frame(tela)
 frConfig.rowconfigure(1,weight=15)
-frConfig.rowconfigure(2,weight=1)
-frConfig.rowconfigure(3,weight=1)
+frConfig.rowconfigure((2,3),weight=1)
 frConfig.grid_columnconfigure(1,weight=2)
-titulo = Label(frConfig,text=" combratec ",font=("Arial",70,"bold"),height=5,bg='#343434',fg='#ffffff')
 
-autor  = Label(frConfig,text="Gabriel Gregório da Silva",font=("Arial",15),bg='#343434',fg='#ffffff')
+titulo = Label(frConfig,text=" combratec ",font=("Arial",70,"bold"),height=5,bg='#343434',fg='#ffffff')
+autores= Label(frConfig,text="Gabriel Gregório da Silva",font=("Arial",15),bg='#343434',fg='#ffffff')
 ano    = Label(frConfig,text="2019",font=("Arial",10),bg='#343434',fg='#ffffff')
+
 titulo.grid(row=1,column=1,sticky=NSEW)
-autor.grid(row=2,column=1,sticky=NSEW)
+autores.grid(row=2,column=1,sticky=NSEW)
 ano.grid(row=3,column=1,sticky=NSEW)
 
 tela.mainloop()
