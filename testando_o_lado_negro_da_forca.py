6b229c2c80f15066a02ac8abafaa05d385fabd19
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2019-09-24 20:28:02 -0300

    Add files via upload

--- testando_o_lado_negro_da_forca.py
+++ testando_o_lado_negro_da_forca.py
@@ -1,83 +1,178 @@
+# analisa se tem comandos e qual procedimento executar
 def analisa_comandos(linha):
-    # analisa se tem comandos e qual procedimento executar
+    # RECEBE CADA LINHA DO PROGRAMA > ' ' + linha
 
-    comandos_disponiveis = [' se ',' espere ',' mostre ',' repita ',' enquanto ',' pare ',' recebe ']
+    comandos = [' se ',
+                ' espere ',
+                ' mostre ',
+                ' exiba ',
+                ' repita ',
+                ' enquanto ',
+                ' pare ',
+                ' recebe ']
+
     for caractere in range(len(linha)):
-        caracteres_restante = len(linha) - caractere
+        total_caracteres_restantes = len(linha) - caractere
 
-        for comando in comandos_disponiveis:
-            if ((linha[  caractere : caractere + len(comando)  ] == comando) and ( len(comando) < caracteres_restante )):
-                if comando == ' se ':
+        for comando in comandos:
+            if ((linha[  caractere : caractere + len(comando)  ] == comando) and ( len(comando) < total_caracteres_restantes )):
+                if (comando == ' se '):
                     verifica_condicionais( ' '+ linha[caractere + len(comando):] + ' ' )
 
-                elif comando == ' espere ':
+                elif (comando == ' espere '):
                     verifica_espere( linha[caractere + len(comando):] )
 
-                elif comando == ' recebe ':
+                elif (comando == ' recebe '):
                     verifica_atribuicoes( linha[caractere + len(comando):] )
 
-                elif comando == ' mostre ':
+                elif ((comando == ' mostre ') or (comando == ' exiba ')) :
                     verifica_validacao_de_valores( linha[caractere + len(comando):] )
 
-
-        #*frases especiais
-        # pense em um número aleatório de
-    pass
-
-def verifica_condicionais(string_condicional):
-    print('verifica_condicionais | {}'.format(string_condicional))
+def verifica_condicionais(condicao):
+    print('<condicional> ',end = '')
     # ************* SO VALE PARA ITEN RELACAO COM ITEN ********************
 
     # ABSTRAIR COMPARADORES
     ignorar_leitura_antes_de = -1
-    comparadores = [' for igual a ',' for diferente de ']
-    operacao = None
-    valor1 = None
-    valor2 = None
-    string = ''
 
-    for caractere in range(len(string_condicional)):
-        # Caso uma condicional seja identificada
+    # COMANDOS DISPONÍVEIS PARA O SE
+    comparadores = [' for igual a ',' for diferente de ',' for maior que ',' for menor que ',' e ',' ou ']
+    operacoes = []
+
+    valor1 = ''
+    valor2 = ''
+    analise1 = False
+    analise2 = False
+
+    for caractere in range(0,len(condicao)):
+        # Caso uma condicional seja identificada (pule ela)
         if caractere < ignorar_leitura_antes_de:
             continue
 
-        caracteres_restante = len(string_condicional) - caractere
+        total_caracteres_restantes = len(condicao) - caractere
 
         # VERIFICAR COMANDO
         for comando in comparadores:
-            if ((string_condicional[caractere:caractere + len(comando)] == comando) and ( len(comando) <= caracteres_restante )):
+            if ((condicao[caractere:caractere + len(comando)] == comando) and ( len(comando) <= total_caracteres_restantes )):
                 if comando == ' for igual a ':
-                    operacao = '=='
+                    if ((analise1 == True) and (analise2 == False)):
+                        analibse1 = False
+                        analise2 = True
+                        operacoes.append('({})'.format(valor1))
+                        valor1 = ''
+
+                    elif ((analise1 == False) and (analise2 == True)):
+                        operacoes.append('({})'.format(valor2))
+                        valor2 = ''
+                        analise2 = False
+
+                    operacoes.append('==')
                     break
 
                 elif comando == ' for diferente de ':
-                    operacao = '!='
+                    if ((analise1 == True) and (analise2 == False)):
+                        analise1 = False
+                        analise2 = True
+                        operacoes.append('({})'.format(valor1))
+                        valor1 = ''
+                    elif ((analise1 == False) and (analise2 == True)):
+                        operacoes.append('({})'.format(valor2))
+                        valor2 = ''
+                        analise2 = False
+                    operacoes.append('!=')
                     break
 
-    if operacao != None:
-        print(operacao)
+                elif comando == ' for menor que ':
+                    if ((analise1 == True) and (analise2 == False)):
+                        analise2 = True
+                        operacoes.append('({})'.format(valor1))
+                        valor1 = ''
+                    elif ((analise1 == False) and (analise2 == True)):
+                        operacoes.append('({})'.format(valor2))
+                        valor2 = ''
+                        analise2 = False
+                    operacoes.append('<')
+                    break
+
+                elif comando == ' e ':
+                    if ((analise1 == True) and (analise2 == False)):
+                        analise2 = True
+                        operacoes.append('({})'.format(valor1))
+                        valor1 = ''
+                    elif ((analise1 == False) and (analise2 == True)):
+                        operacoes.append('({})'.format(valor2))
+                        valor1 = ''
+                        valor2 = ''
+                        analise2 = False
+                    operacoes.append('e')
+                    break
+                elif comando == ' ou ':
+                    if ((analise1 == True) and (analise2 == False)):
+                        analise1 = False
+                        analise2 = True
+                        operacoes.append('({})'.format(valor1))
+                        valor1 = ''
+                    elif ((analise1 == False) and (analise2 == True)):
+                        operacoes.append('({})'.format(valor2))
+                        valor2 = ''
+                        analise2 = False
+                    operacoes.append('ou')
+                    break
+
+        if ((analise1 == False) and (analise2 == False)):
+            analise1 == True
+
+        if ((analise1 == True) and (analise2 == False)):
+            valor1 += condicao[caractere]
+
+        elif ((analise1 == False) and (analise2 == True)):
+            valor2 += condicao[caractere]
+
+
+    if operacoes != []:
+        for operacao in operacoes:    
+
+            if operacao == '==':
+                print('<igualdade> ',end='')        
+
+            elif operacao == '<':
+                print('<menor> ',end='')    
+
+            elif operacao == '!=':
+                print('<diferença> ',end='')    
+
+            elif operacao == 'e':
+                print('<AND> ',end='')    
+
+            elif operacao == 'ou':
+                print('<OR> ',end='')    
+
+            elif ('(' in operacao):
+                print('({}) '.format(operacao),end='')
+
     else:
         print('Não foi definida uma condição')
+    print('')
 
-    # se        >>CONDICAO<<
-    # enquanto  >>CONDICAO<<
-    # verifica se os valores são condicionais
-    # se não forem, retorna a linha do erro
 
 def abstrair_valores_de_variaveis(variavel):
     pass
 
 def verifica_validacao_de_valores(finalização):
-    print('verifica e valida finalização | {}'.format(finalização))
+    print('[OK] valores > ',end = '')
+    print('')
+
     # verifica e resolve conflitos para retornar um único valor
 
 def verifica_atribuicoes(valor):
-    print('verifica atribuicoes | {}'.format(valor))
+    print('[OK] Atribuições > ',end = '')
     # objeto recebe VALOR
     # Verifica se o valor é consinstente ou não
+    print('')
 
 def verifica_espere(finalização):
-    print('verifique o espere | {}'.format(finalização))
+    print('[OK] espere > ',end = '')
+    print('')
     #espere 2 SEGUNDOS
     # analisa se o valor é válido e se o o que vem depois também
 
@@ -93,7 +188,7 @@ def verifica_aleatorio():
 
 linhas = '''mundo recebe 'x'
 mostre "mundo"
-se for igual a 4
+se 7 for igual a 4 e 5 for diferente de 6 ou 4 for menor que 8
 se 4 for diferente de 3
 espere 4 segundos'''.split('\n')
 
