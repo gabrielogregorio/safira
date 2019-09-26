27d354af5fbe3ae60ea15166e02330a7474d529b
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2019-09-26 19:09:14 -0300

    Add files via upload

--- linter.py
+++ linter.py
@@ -2,8 +2,7 @@ global variaveis
 variaveis = {}
 
 # analisa se tem comandos e qual procedimento executar
-def analisa_comandos(linha):
-    # RECEBE CADA LINHA DO PROGRAMA > ' ' + linha
+def analisa_comandos(linha,num_linha):
 
     comandos = [' se ',
                 ' espere ',
@@ -15,38 +14,40 @@ def analisa_comandos(linha):
                 ' recebe ',
                 ' vale ']
 
+    eventos = False
     for caractere in range(len(linha)):
         total_caracteres_restantes = len(linha) - caractere
 
         for comando in comandos:
             if ((linha[  caractere : caractere + len(comando)  ] == comando) and ( len(comando) < total_caracteres_restantes )):
+                eventos = True
                 if (comando == ' se '):
-                    verifica_condicionais( ' '+ linha[caractere + len(comando):] + ' ' )
+                    verifica_condicionais(' '+ linha[caractere + len(comando):] + ' ',num_linha )
 
                 if (comando == ' enquanto '):
-                    verifica_loop_enquanto( ' '+ linha[caractere + len(comando):] + ' ' )
-
-                    
+                    verifica_loop_enquanto( ' '+ linha[caractere + len(comando):] + ' ',num_linha )
 
                 elif (comando == ' espere '):
-                    verifica_espere( linha[caractere + len(comando):] )
+                    verifica_espere( linha[caractere + len(comando):] , num_linha)
 
                 elif (comando == ' recebe ' or comando == ' vale '):
                     valor_variavel = linha[caractere + len(comando):]
                     variavel = linha[:caractere]
-                    definir_variaveis(variavel,valor_variavel)
 
-                elif ((comando == ' mostre ') or (comando == ' exiba ')) :
-                    verifica_validacao_de_valores( linha[caractere + len(comando):] )
+                    definir_variaveis(variavel,valor_variavel,num_linha)
 
+                elif ((comando == ' mostre ') or (comando == ' exiba ') or (comando == ' exiba nessa linha') or (comando == ' mostre nessa linha ')):
+                    verifica_mostre( linha[caractere + len(comando):],num_linha )
 
-# Recebe algo e retorna um valor
-# recebe o valor pelado, sem espaços na lateral
+    if linha == '' or linha.isspace():
+        pass
+    elif eventos == False:
+        print('[{}] O INTERPRETADOR NÂO ENTEDEU O QUE VOCÊ QUIZ DIZER'.format(num_linha))
+
 def abstrai_valor(objeto):
     # é uma string?
     if objeto[0] == '"' and objeto[-1] == '"':
-        print('objeto é uma string!')
-        return objeto[1,-1]
+        return [True , objeto[1:-1]]
 
     # é um número
     elif objeto.isnumeric():
@@ -54,13 +55,10 @@ def abstrai_valor(objeto):
 
     # é uma variável
     else:
-        resultado = descobrir_valor_variavel(objeto)
+        return descobrir_valor_variavel(objeto)
 
-    return resultado
-
-def verifica_condicionais(condicao):
+def verifica_condicionais(condicao,num_linha):
     print('<condicional> ',end = '')
-    # COMANDOS DISPONÍVEIS PARA O SE
     comandos_comparadores = [' for igual a ',' for diferente de ',' for maior que ',' for menor que ',' e ',' ou ']
     operacoes = []
     ignorar_leitura_antes_de = -1
@@ -68,18 +66,15 @@ def verifica_condicionais(condicao):
     analise_valor = False
 
     for caractere in range(0,len(condicao)):
-        # Caso uma condicional seja identificada (pule ela)
+        # CONDIÇÃO IDENTIFICADA, PULE ELA
         if caractere < ignorar_leitura_antes_de:
             continue
 
         total_caracteres_restantes = len(condicao) - caractere
         encontrou_comando = False
 
-        # VERIFICAR COMANDO
         for comando in comandos_comparadores:
-            # COMANDO ENCONTRADO
             if ((condicao[caractere:caractere + len(comando)] == comando) and ( len(comando) <= total_caracteres_restantes )):
-                # O QUE FAZER, DEPENDENDO DO COMANDO
                 if (analise_valor == True):
                     analise_valor = False
                     operacoes.append('({})'.format(valor))
@@ -111,7 +106,6 @@ def verifica_condicionais(condicao):
                     break
             
         if encontrou_comando == False:
-
             if (analise_valor == False):
                 analise_valor = True
 
@@ -152,6 +146,7 @@ def verifica_condicionais(condicao):
 
 def descobrir_valor_variavel(variavel):
     global variaveis
+    variavel = variavel.strip()
     try:
         valor = variaveis[variavel]
     except:
@@ -160,14 +155,19 @@ def descobrir_valor_variavel(variavel):
         return [True,valor]
 
 
-def definir_variaveis(variavel , valor_variavel):
+def definir_variaveis(variavel , valor_variavel , num_linha):
     global variaveis
+    valor = valor_variavel.strip()
     variavel = variavel.strip()
-    variaveis[variavel] = valor_variavel
-    print('[OK] VARIAVEL {} SETADA para {}> '.format(variavel,valor_variavel),end = '')
+
+    if (abstrai_valor(valor)[0]) == False:
+        print('[{}] IMPOSIVEL DEFINIR VARIAVEL {}, PORQUE A VARIAVEL {} NÃO FOI DEFINIDA ANTERIORMENTE'.format(num_linha,variavel,valor_variavel))
+    else:
+        variaveis[variavel] = valor
+        print('[{}] VARIAVEL {} SETADA para {}> '.format(num_linha,variavel,valor_variavel),end = '')
     print('')
 
-def verifica_espere(condicao):
+def verifica_espere(condicao,num_linha):
     print('<ESPERE> ',end = '')
     condicao = ' ' + condicao.strip() + ' '
 
@@ -227,7 +227,7 @@ def verifica_espere(condicao):
     print('')
 
 
-def verifica_loop_enquanto (condicao):
+def verifica_loop_enquanto (condicao,num_linha):
     print('<enquanto> ',end = '')
     # COMANDOS DISPONÍVEIS PARA O SE
     comandos_comparadores = [' for igual a ',' for diferente de ',' for maior que ',' for menor que ',' e ',' ou ']
@@ -310,7 +310,8 @@ def verifica_loop_enquanto (condicao):
                 print('<OR > ',end='')    
 
             elif '(' in operacao:
-                valor = abstrai_valor((operacao[1:-1].strip()))
+
+                valor = abstrai_valor((operacao[0:].strip()))
                 if valor[0] == False:
                     print(' ##ERRO## ',end='')
                 else:
@@ -319,50 +320,64 @@ def verifica_loop_enquanto (condicao):
         print('[ERRO] Não foi definida nenhuma condição')
     print('')
 
+def verifica_mostre(condicao,num_linha):
+    print('[{}] <mostre> '.format(num_linha),end = '')
 
+    operacoes = []
+    valor = ''
 
+    marcador = False
+    indicador_outro = False
 
+    for caractere in range(0,len(condicao)):
+        if  ((condicao[caractere] == '"' or caractere + 1 == len(condicao) ) and marcador == True)  and indicador_outro == False:
+            marcador = False
+            operacoes.append('"{}"'.format(valor))
+            valor = ''
 
+        elif marcador == True and indicador_outro == False:
+            valor += condicao[caractere]
 
+        elif  condicao[caractere] == '"' and marcador == False and indicador_outro == False:
+            marcador = True
 
+        elif indicador_outro == False:
+            valor += condicao[caractere]
+            indicador_outro = True
 
+        elif (condicao[caractere] == ' ' or caractere+1 == len(condicao))  and indicador_outro == True:
+            indicador_outro = False
+            valor += condicao[caractere]
+            operacoes.append('{}'.format(valor))
+            valor = ''
 
+        elif indicador_outro == True:
+            valor += condicao[caractere]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    valor = []
+    if operacoes != []:
+        for operacao in operacoes:
+            valor = abstrai_valor((operacao.strip()))
+            if valor[0] == False:
+                print(' ##ERRO## ',end='')
+            else:
+                print('__valor__ ',end='')
+    print('')
 
 linhas = '''
 
-nome vale 4
-espere nome segundos
+gabriel vale "1234"
 
-x recebe 3
 
-enquanto x for igual a 14 ou tpm for menor que 6
 
+gregorio receb gabriel
+nasa vale gregorio
+gregorio"gregorio" vale 13
+
+mostre gabriel gregorio"gregorio" nasa
+ 
 '''.split('\n')
 
-for linha in linhas:
+for num_linha in range(len(linhas)):
     # garanta 1 espaço antes de cada linha
-    analisa_comandos(' ' + linha)
+    analisa_comandos(' ' + linhas[num_linha],num_linha)
