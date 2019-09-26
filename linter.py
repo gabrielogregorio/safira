ac39244f1b678df0662adf61f31fab5b0aa1d3f6
Gabriel Gregório da Silva <48621655+gabrielogregorio@users.noreply.github.com>
2019-09-26 16:14:49 -0300

    Add files via upload

--- linter.py
+++ linter.py
@@ -23,6 +23,11 @@ def analisa_comandos(linha):
                 if (comando == ' se '):
                     verifica_condicionais( ' '+ linha[caractere + len(comando):] + ' ' )
 
+                if (comando == ' enquanto '):
+                    verifica_loop_enquanto( ' '+ linha[caractere + len(comando):] + ' ' )
+
+                    
+
                 elif (comando == ' espere '):
                     verifica_espere( linha[caractere + len(comando):] )
 
@@ -33,6 +38,25 @@ def analisa_comandos(linha):
 
                 elif ((comando == ' mostre ') or (comando == ' exiba ')) :
                     verifica_validacao_de_valores( linha[caractere + len(comando):] )
+
+
+# Recebe algo e retorna um valor
+# recebe o valor pelado, sem espaços na lateral
+def abstrai_valor(objeto):
+    # é uma string?
+    if objeto[0] == '"' and objeto[-1] == '"':
+        print('objeto é uma string!')
+        return objeto[1,-1]
+
+    # é um número
+    elif objeto.isnumeric():
+        return [True,objeto]
+
+    # é uma variável
+    else:
+        resultado = descobrir_valor_variavel(objeto)
+
+    return resultado
 
 def verifica_condicionais(condicao):
     print('<condicional> ',end = '')
@@ -98,12 +122,9 @@ def verifica_condicionais(condicao):
 
         encontrou_comando = False
 
-
-
-
-
     if operacoes != []:
         for operacao in operacoes:  
+
             if operacao == '==':
                 print('<IGUALDADE> ',end='')        
 
@@ -120,31 +141,24 @@ def verifica_condicionais(condicao):
                 print('<OR > ',end='')    
 
             elif '(' in operacao:
-                print('__valor__ ',end='')
+                valor = abstrai_valor((operacao[1:-1].strip()))
+                if valor[0] == False:
+                    print(' ##ERRO## ',end='')
+                else:
+                    print('__valor__ ',end='')
     else:
-        print('Não foi definida uma condição')
+        print('[ERRO] Não foi definida nenhuma condição')
     print('')
-
-
-
 
 def descobrir_valor_variavel(variavel):
     global variaveis
     try:
         valor = variaveis[variavel]
     except:
-        print('[ERRO] A variavel {} não foi definida!'.format(variavel))
-        return '[ERRO] A variavel {} não foi definida!'.format(variavel)
+        return [False,'[ERRO] A variavel {} não foi definida!'.format(variavel)]
     else:
-        print(valor)
-        return valor
+        return [True,valor]
 
-
-def verifica_validacao_de_valores(finalização):
-    print('[OK] valores > ',end = '')
-    print('')
-
-    # verifica e resolve conflitos para retornar um único valor
 
 def definir_variaveis(variavel , valor_variavel):
     global variaveis
@@ -153,29 +167,200 @@ def definir_variaveis(variavel , valor_variavel):
     print('[OK] VARIAVEL {} SETADA para {}> '.format(variavel,valor_variavel),end = '')
     print('')
 
-def verifica_espere(finalização):
-    print('[OK] espere > ',end = '')
+def verifica_espere(condicao):
+    print('<ESPERE> ',end = '')
+    condicao = ' ' + condicao.strip() + ' '
+
+    # COMANDOS DISPONÍVEIS PARA O SE
+    comandos_comparadores = [' segundos ',' segundo ',' s ']
+    operacoes = []
+    ignorar_leitura_antes_de = -1
+    valor = ''
+    analise_valor = False
+
+    for caractere in range(0,len(condicao)):
+        # Caso uma condicional seja identificada (pule ela)
+        if caractere < ignorar_leitura_antes_de:
+            continue
+
+        total_caracteres_restantes = len(condicao) - caractere
+        encontrou_comando = False
+
+        # VERIFICAR COMANDO
+        for comando in comandos_comparadores:
+            # COMANDO ENCONTRADO
+            if ((condicao[caractere:caractere + len(comando)] == comando) and ( len(comando) <= total_caracteres_restantes )):
+                # O QUE FAZER, DEPENDENDO DO COMANDO
+                if (analise_valor == True):
+                    analise_valor = False
+                    operacoes.append('({})'.format(valor))
+                    valor = ''
+
+                if ((comando == ' segundo ') or (comando == ' segundos ') or (comando == ' s ')) :
+                    operacoes.append('segundos')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+
+        if encontrou_comando == False:
+            if (analise_valor == False):
+                analise_valor = True
+
+            if (analise_valor == True):
+                valor += condicao[caractere]
+                if total_caracteres_restantes == 1:
+                    operacoes.append('({})'.format(valor))
+
+        encontrou_comando = False
+
+    if operacoes != []:
+        for operacao in operacoes:  
+            if operacao == 'segundos':
+                print('<UNIDADE> ',end='')        
+            elif '(' in operacao:
+                valor = abstrai_valor((operacao[1:-1].strip()))
+                if valor[0] == False:
+                    print(' ##ERRO## ',end='')
+                else:
+                    print('__valor__ ',end='')
+    else:
+        print('[ERRO] Não foi definida nenhuma condição')
     print('')
-    #espere 2 SEGUNDOS
-    # analisa se o valor é válido e se o o que vem depois também
 
-def verifica_loop_especial ():
-    # repita 20 VEZES
-    # verifica o valor e o VEZES
-    pass
 
-def verifica_aleatorio():
-    #verifica se os valores são válidos, pode ser string, de 'a' a 'b'
-    #pense em um número aleatório de 2 a 20
-    pass
+def verifica_loop_enquanto (condicao):
+    print('<enquanto> ',end = '')
+    # COMANDOS DISPONÍVEIS PARA O SE
+    comandos_comparadores = [' for igual a ',' for diferente de ',' for maior que ',' for menor que ',' e ',' ou ']
+    operacoes = []
+    ignorar_leitura_antes_de = -1
+    valor = ''
+    analise_valor = False
 
-#linhas = '''mundo recebe 'x'
-#mostre "mundo"
-linhas = '''se numero0 for igual a numero1 ou numero2 for igual a numero3
-se numero1 for igual a numero2 e numero3 for diferente de numero4 ou numero5 for menor que numero6
-se numero7 for diferente de numero8
-espere numero9 segundos
-nome recebe 'gabriel' 
+    for caractere in range(0,len(condicao)):
+        # Caso uma condicional seja identificada (pule ela)
+        if caractere < ignorar_leitura_antes_de:
+            continue
+
+        total_caracteres_restantes = len(condicao) - caractere
+        encontrou_comando = False
+
+        # VERIFICAR COMANDO
+        for comando in comandos_comparadores:
+            # COMANDO ENCONTRADO
+            if ((condicao[caractere:caractere + len(comando)] == comando) and ( len(comando) <= total_caracteres_restantes )):
+                # O QUE FAZER, DEPENDENDO DO COMANDO
+                if (analise_valor == True):
+                    analise_valor = False
+                    operacoes.append('({})'.format(valor))
+                    valor = ''
+
+                if comando == ' for igual a ':
+                    operacoes.append('==')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+
+                elif comando == ' for diferente de ':
+                    operacoes.append('!=')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+
+                elif comando == ' for menor que ':
+                    operacoes.append('<')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+
+                elif comando == ' e ':
+                    operacoes.append('e')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+
+                elif comando == ' ou ':
+                    operacoes.append('ou')
+                    [ ignorar_leitura_antes_de , encontrou_comando ] = [caractere + len(comando),True]
+                    break
+            
+        if encontrou_comando == False:
+
+            if (analise_valor == False):
+                analise_valor = True
+
+            if (analise_valor == True):
+                valor += condicao[caractere]
+                if total_caracteres_restantes == 1:
+                    operacoes.append('({})'.format(valor))
+
+        encontrou_comando = False
+
+    if operacoes != []:
+        for operacao in operacoes:  
+
+            if operacao == '==':
+                print('<IGUALDADE> ',end='')        
+
+            elif operacao == '<':
+                print('<MENOR> ',end='')    
+
+            elif operacao == '!=':
+                print('<DIFERENCA> ',end='')    
+
+            elif operacao == 'e':
+                print('<AND> ',end='')    
+
+            elif operacao == 'ou':
+                print('<OR > ',end='')    
+
+            elif '(' in operacao:
+                valor = abstrai_valor((operacao[1:-1].strip()))
+                if valor[0] == False:
+                    print(' ##ERRO## ',end='')
+                else:
+                    print('__valor__ ',end='')
+    else:
+        print('[ERRO] Não foi definida nenhuma condição')
+    print('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+linhas = '''
+
+nome vale 4
+espere nome segundos
+
+x recebe 3
+
+enquanto x for igual a 14 ou tpm for menor que 6
+
 '''.split('\n')
 
 for linha in linhas:
