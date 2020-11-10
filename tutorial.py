bdbe3383a4f5a930efbd391c681ceaefdb01a115
Gabriel Gregório da Silva <gabriel.gregorio.1@outlook.com>
2020-11-10 01:02:22 -0300

    Tutorial embutido

---
+++ tutorial.py
@@ -0,0 +1,63 @@
+Recurso para ensinar o usuário passo a passo para iniciar no mundo da programação.
+
+# Primeiro starting da interface
+* Escolha seu idioma
+* Escolha um tema
+* Escolha um tamanho legal de fonte
+* Escolha um modo
+    Quero aprender a programar!
+    Só quero explorar
+
+
+# Modo aprender a programar
+----------------
+
+Olá, seja bem vindo, meu nome é safira, e vou te ensinar a programar!
+<abandonar> <vamos lá!>
+
+
+----------------
+
+Primeiro, vamos começar com o básico, o que é uma linguagem de programação?
+
+Uma linguagem de programação....
+É como.....
+
+<sair> <ver um vídeo> <entendido e anotado!>
+
+----------------
+
+E para que serve uma linguagem de programação?
+
+Uma linguagem de programação serve para....
+.....sites, jogos, aplicativos, etc....
+
+<sair> <ver um vídeo> <entendido e anotado!>
+
+----------------
+
+Agora vamos falar sobre mim
+
+Eu sou uma interface, isto é, um programa(?) que vai ter ajudar a iniciar no mundo da programação.
+
+
+<sair> <> <entendido e anotado!>
+
+----------------
+Isto ai, clique na guia arquivos. Guia são aqueles botões na parte de cima da safira.
+
+Muito bom, estas são as opções.
+1. Abri Arquivo.
+	Serve para .....
+2............
+	Serve para .....
+3. Abri Arquivo.
+	Serve para .....
+4...........
+	Serve para .....
+5 Abri Arquivo.
+
+<sair> <pular explicações da interface> <entendido e anotado!>
+
+----------------
+
